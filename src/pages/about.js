d770351dd615b5ffc67d1e4b782351e258045339
Thomas McBride <thomasmcbride@Thomass-MacBook-Pro.local>
2021-04-08 13:19:48 -0700

    adding about info

---
+++ src/pages/about.js
@@ -0,0 +1,38 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import Typography from "@material-ui/core/Typography"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="About First Pull" />
+    <Typography variant="h3" align="center" display="block">
+      About First Pull
+    </Typography>
+    <Typography variant="h5">Meet the Leaders</Typography>
+    <Typography variant="h6">Guild Leaders</Typography>
+    <Typography variant="p" display="block" paragraph>
+      Yergory, Fallenslayer
+    </Typography>
+    <Typography variant="h6">Officers</Typography>
+    <Typography variant="p" display="block" paragraph>
+      Replay, Pheb
+    </Typography>
+    <Typography variant="h6">Main Raiders:</Typography>
+    <Typography variant="p" display="block">
+      <b>Tanks:</b>Pheb, Kaifel
+    </Typography>
+    <Typography variant="p" display="block">
+      <b>DPS:</b>Fallenslayêr, Nevafox, Replay, Slothraxx, Puttris, Kiedra,
+      Ninjayoder, Fawkes, Gamerboypee, Elstra
+    </Typography>
+    <Typography variant="p" display="block" paragraph>
+      <b>Healers:</b>Yergory, Tankfist, Mespropp
+    </Typography>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default SecondPage
